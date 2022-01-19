7b68e4694582c5f3e12f8485a85b49eb176b027d
Daywick <derekli11204@gmail.com>
2022-01-18 19:34:10 -0500

    css and bootstrap mainly, started adding new ideas

--- SocialSite/Accounts/models.py
+++ SocialSite/Accounts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib import auth
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class User(auth.models.User,auth.models.PermissionsMixin):
+
     #return username with @ for social media "aesthetics"
     def __str__(self):
         return "@{}".format(self.username)
+
--- SocialSite/Groups/models.py
+++ SocialSite/Groups/models.py
@@ -21,7 +21,7 @@ class Group(models.Model):
     """
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(allow_unicode=True, unique=True)
-    description = models.TextField(blank=True, default='Enter Default Text Here')
+    description = models.TextField(blank=True, default='Enter Text Here')
     description_html = models.TextField(editable=False,default='',blank=True)
     members = models.ManyToManyField(User,through='GroupMember')
 
