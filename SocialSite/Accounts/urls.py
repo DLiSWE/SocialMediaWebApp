d09d84ce2c6a32486014aa7a02951a6bdef8f932
Daywick <derekli11204@gmail.com>
2022-01-19 05:33:29 -0500

    Edit user page base page set up, needs formatting

--- SocialSite/Accounts/urls.py
+++ SocialSite/Accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -7,5 +7,6 @@ app_name = 'Accounts'
 urlpatterns = [
     re_path(r'^login/$', auth_views.LoginView.as_view(template_name='Accounts/login.html'),name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    re_path(r'^signup/$', views.SignUp.as_view(), name='signup')
+    re_path(r'^signup/$', views.SignUp.as_view(), name='signup'),
+    re_path(r'^edit_profile/$', views.EditUser.as_view(), name='editprof'),
 ]
