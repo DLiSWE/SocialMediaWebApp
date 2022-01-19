8e8186b94d89dfe899ba7de755b13e297f4c8762
Daywick <derekli11204@gmail.com>
2022-01-19 05:15:29 -0500

    fixed some css, added search bar(not working yet), started creating view for edit user profile

--- SocialSite/Accounts/views.py
+++ SocialSite/Accounts/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from . import forms
+from django.contrib.auth.forms import UserChangeForm
 # Create your views here.
 
 class SignUp(CreateView):
     form_class = forms.UserSignupForm
-    success_url = reverse_lazy('login')
     #we use reverse lazy over here instead of lazy because we don't want to
     #execute before signing up
     template_name = 'Accounts/signup.html'
+    success_url = reverse_lazy('login')
+
+class EditUser(CreateView):
+    #temporary form
+    form_class = UserChangeForm
+    template_name = 'Accounts/edit_profile.html'
+    success_url = reverse_lazy('home')
